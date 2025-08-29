dcba0e5ae115f1cd5fcc582b5c6d540e6a64c1b8
Milad Fadavvi <Fadavvi@users.noreply.github.com>
2025-08-29 14:47:13 +0000

    v1.0.4
    Payload list

--- gamal.py
+++ gamal.py
@@ -14,7 +14,7 @@ os.makedirs(UploadPath, exist_ok=True)
 #------------------------------------------
 @app.route('/info', methods=['GET','POST']) # Print info 
 def info():
-    return "<h3>Gamal v1.0.3 | Milad Fadavvi</h3>" 
+    return "<h3>Gamal v1.0.4 | Milad Fadavvi</h3>" 
 #------------------------------------------
 @app.route('/f/<path:path>') #Servs files
 def send_js(path):
@@ -84,27 +84,46 @@ def SaveData(e='',path=''):
     LogFile.close()
     return '<html><b>{}</b></html>'.format(Arguments.canary) , 200
 #------------------------------------------
+############# Payloads list ########################
+def PrirntFileURLs(path: str, hostname: str, port: int):
+    if not os.path.exists(path):
+        print(f"The path [./f/] does not exist.")
+        return
+    for root, _, files in os.walk(path):
+        for file in files:
+            RelPath = os.path.relpath(os.path.join(root, file), start=path)
+            url = f"https://{hostname}:{port}/f/{RelPath.replace(os.sep, '/')}"
+            print(url)
 ############# Main Function ########################
 if __name__ == "__main__":
     global Arguments #!!!
     #------------------------------------------
-    parser = argparse.ArgumentParser(description='Gamal v1.0.3:\nA tiny flask app for helping red-teamers, purple teamers, and pentesters in delivery, data exfiltration, and some attacks (SSRF, XXE, XSS, Session Hijacking, Session Riding).\n\n')
+    parser = argparse.ArgumentParser(description='Gamal v1.0.4:\nA tiny flask app for helping red-teamers, purple teamers, and pentesters in delivery, data exfiltration, and some attacks (SSRF, XXE, XSS, Session Hijacking, Session Riding).\n\n')
     parser.add_argument('--log', default='gamal.log' , help='Path to the log file')
     parser.add_argument('--port', default=1337 , help='Port / HTTPs')
     parser.add_argument('--ip', default='0.0.0.0' , help='IP e.g. : 0.0.0.0 or 127.0.0.1')
     parser.add_argument('--canary', default='booqbooqGamal' , help='Canary token')
     parser.add_argument('--cert', help='Your fullchain.pem file')
     parser.add_argument('--key', help='Your ssl private key file')
+    parser.add_argument('--host', help='Your hostname or external IP add')
     Arguments = parser.parse_args()
     #------------------------------------------
     if Arguments.cert and Arguments.key:
         cert_file = Arguments.cert
         key_file = Arguments.key
+        if Arguments.host:
+            PrirntFileURLs('./f', Arguments.host, Arguments.port)
+        else:
+            PrirntFileURLs('./f', Arguments.ip, Arguments.port)
         context = ssl.SSLContext(ssl.PROTOCOL_TLS)
         context.load_cert_chain(certfile=cert_file, keyfile=key_file)
         app.run(host=Arguments.ip , port=int(Arguments.port),threaded=True, ssl_context=context)
     else: 
         try:
+            if Arguments.host:
+                PrirntFileURLs('./f', Arguments.host, Arguments.port)
+            else:
+                PrirntFileURLs('./f', Arguments.ip, Arguments.port)
             app.run(host=Arguments.ip , port=int(Arguments.port),threaded=True, ssl_context='adhoc')
         except:
             parser.print_help()
