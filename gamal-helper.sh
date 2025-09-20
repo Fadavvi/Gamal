93ca924335c8ced74aa7b039afd1c823781dc0d8
Milad Fadavvi <Fadavvi@users.noreply.github.com>
2025-09-20 10:57:13 +0000

    Clean up!

--- gamal-helper.sh
+++ gamal-helper.sh
@@ -1,85 +1,109 @@
 #!/usr/bin/env bash
 ####################################################
 echo "Run 'sudo apt install p7zip-full wget curl -y' first!"
+echo "----------------------"
 ####################################################
 cd f
 mkdir --parents win linux mac web
 ####################################################
 cd win
+echo "Windows:"
 
+echo "*- Mimikatz"
 wget --quiet https://github.com/gentilkiwi/mimikatz/releases/latest/download/mimikatz_trunk.zip
 7z x mimikatz_trunk.zip -o./mimikatznk &> /dev/null
 mv mimikatznk/x64/ mimikatz64
 rm -rf mimikatznk/ mimikatz_trunk.zip
 
+echo "*- SharpCollection"
 wget --quiet https://github.com/Flangvik/SharpCollection/archive/refs/heads/master.zip
 7z x master.zip > /dev/null
 mv SharpCollection-master/NetFramework_4.7_x64 sharpc64
 rm -rf SharpCollection-master master.zip
 
+echo "*- ligolo-ng"
 curl -s https://api.github.com/repos/nicocha30/ligolo-ng/releases/latest | grep "browser_download_url.*windows_amd64*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-find . -type f -name "*.zip" -exec 7z x {} -o* \;
+find . -type f -name "*.zip" -exec 7z x {} -o* \; &> /dev/null
 mkdir --parents ligolo
 mv ligolo-ng_agent*/agent.exe ligolo/agent.exe
 mv ligolo-ng_proxy*/proxy.exe ligolo/proxy.exe
 rm -rf ligolo-ng_agent* ligolo-ng_proxy*
 
+echo "*- PEASS-ng"
 curl -s https://api.github.com/repos/peass-ng/PEASS-ng/releases/latest | grep "browser_download_url.*winPEAS*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 mkdir --parents peass
 mv winPEAS* peass
 
+echo "*- AzureHound"
 curl -s https://api.github.com/repos/SpecterOps/AzureHound/releases/latest | grep "browser_download_url.*windows_amd64*" | cut -d : -f 2,3 | tr -d \" | head -n 1 | wget -qi -
-7z x AzureHound*.zip
+7z x AzureHound*.zip &> /dev/null
 rm -f AzureHound*.zip
 
+echo "*- ncat"
 wget --quiet https://nmap.org/dist/ncat-portable-5.59BETA1.zip
-7z x ncat-portable-5.59BETA1.zip
+7z x ncat-portable-5.59BETA1.zip &> /dev/null
 mv ncat-portable-5.59BETA1/ncat.exe .
-rm -rf ncat-portable-5.59BETA1
+rm -rf ncat-portable-5.59BETA1*
 
+echo "----------------------"
 cd ..
 ####################################################
 cd linux
+echo "Linux:"
 
+echo "*- PEASS-ng"
 curl -s https://api.github.com/repos/peass-ng/PEASS-ng/releases/latest | grep "browser_download_url.*linpeas_linux*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 mkdir --parents peass
 mv linpeas* peass
 curl -s https://api.github.com/repos/peass-ng/PEASS-ng/releases/latest | grep "browser_download_url.*linpeas*.sh" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 mv linpeas* peass
 
+echo "*- ligolo-ng"
 curl -s https://api.github.com/repos/nicocha30/ligolo-ng/releases/latest | grep "browser_download_url.*linux_amd64*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 mkdir --parents ligolo
-find . -type f -name "*.tar.gz" -exec tar -xvf {} -C ligolo \;
+find . -type f -name "*.tar.gz" -exec tar -xvf {} -C ligolo \; &> /dev/null
 rm -f ligolo/README.md ligolo/LICENSE
+rm -f ligolo-ng*.tar.gz
 
+echo "*- AzureHound"
 curl -s https://api.github.com/repos/SpecterOps/AzureHound/releases/latest | grep "browser_download_url.*linux_amd64*" | cut -d : -f 2,3 | tr -d \" | head -n 1 | wget -qi -
-7z x AzureHound*.zip
+7z x AzureHound*.zip &> /dev/null
 rm -f AzureHound*.zip
 
+echo "*- traitor"
 curl -s https://api.github.com/repos/liamg/traitor/releases/latest | grep "browser_download_url.*amd64*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 
+echo "----------------------"
 cd ..
 ####################################################
 cd mac
+echo "macOS:"
 
+echo "*- PEASS-ng"
 curl -s https://api.github.com/repos/peass-ng/PEASS-ng/releases/latest | grep "browser_download_url.*darwin_amd64" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 mkdir --parents peass
 mv linpeas* peass
 
+echo "*- ligolo-ng"
 curl -s https://api.github.com/repos/nicocha30/ligolo-ng/releases/latest | grep "browser_download_url.*darwin_amd64.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 mkdir --parents ligolo
-find . -type f -name "*.tar.gz" -exec tar -xvf {} -C ligolo \;
+find . -type f -name "*.tar.gz" -exec tar -xvf {} -C ligolo \; &> /dev/null
 rm -f ligolo/README.md ligolo/LICENSE ligolo-ng*.tar.gz
 
-curl -s https://api.github.com/repos/SpecterOps/AzureHound/releases/latest | grep "browser_download_url.*darwin*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-7z x AzureHound*.zip
+echo "*- AzureHound"
+curl -s https://api.github.com/repos/SpecterOps/AzureHound/releases/latest | grep "browser_download_url.*darwin_amd64.zip" | grep -v sha256  | cut -d : -f 2,3 | tr -d \" | wget -qi -
+7z x AzureHound*.zip &> /dev/null
 rm -f AzureHound*.zip
 
+echo "----------------------"
 cd ..
 ####################################################
 cd web
+echo "Web-Shells"
 
 curl -s https://raw.githubusercontent.com/BlackArch/webshells/refs/heads/master/asp/cmdasp.asp -o cmdasp.asp
 curl -s https://raw.githubusercontent.com/BlackArch/webshells/refs/heads/master/aspx/cmdasp.aspx -o cmdasp.aspx
 curl -s https://raw.githubusercontent.com/BlackArch/webshells/refs/heads/master/jsp/cmdjsp.jsp -o cmdjsp.jsp 
 curl -s https://raw.githubusercontent.com/BlackArch/webshells/refs/heads/master/php/pws.php -o cmdphp.php
+
+echo "Done! Now, Hack the planet. 8-)"
